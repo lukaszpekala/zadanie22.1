f7eef79fa060ad54e32a16c1917dff09c38ed392
lukaszpekala <lukasz.pekala.pl@gmail.com>
2022-01-06 22:03:31 +0100

    poprawki, wydzielenie klasy do wysylania maili

--- src/main/java/com/example/zadanie22_1/MainController.java
+++ src/main/java/com/example/zadanie22_1/MainController.java
@@ -1,8 +1,6 @@
 package com.example.zadanie22_1;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MainController {
 
-    private static final String COMPANY_MAIL = "contact@company.com";
-    private static final String MAIL_SUBJECT = "Wiadomość od ";
-    private static final String CONFIRMATION_MAIL_SUBJECT = "Dziękujemy za wiadomość";
-    private static final String CONFIRMATION_MAIL_TEXT =
-            "Odpowiemy możliwie najszybciej. Prosimy nie odpowiadać na tą wiadomość automatyczną!";
+    private final MailService mailService;
 
-    @Autowired
-    private JavaMailSender mailSender;
-
+    public MainController(MailService mailService) {
+        this.mailService = mailService;
+    }
 
     @GetMapping("/")
     public String home() {
@@ -35,27 +29,7 @@ public class MainController {
 
     @PostMapping("/send")
     public String send(Message message) {
-        mailSender.send(createContactMail(message));
-        mailSender.send(createConfirmationMail(message));
+        mailService.sendContactAndConfirmationMail(message);
         return "redirect:/";
-    }
-
-    private SimpleMailMessage createContactMail(Message message) {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(COMPANY_MAIL);
-        mail.setSubject(MAIL_SUBJECT + message.getName());
-        mail.setText(message.getText());
-        mail.setReplyTo(message.getEmail());
-        mail.setFrom(message.getName());
-        return mail;
-    }
-
-    private SimpleMailMessage createConfirmationMail(Message message) {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(message.getEmail());
-        mail.setSubject(CONFIRMATION_MAIL_SUBJECT);
-        mail.setText(CONFIRMATION_MAIL_TEXT);
-        mail.setFrom(COMPANY_MAIL);
-        return mail;
     }
 }
